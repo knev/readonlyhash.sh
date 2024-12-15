ffefb737233e304d415b75d1831e1cf025e9abfc
knev <knev@users.noreply.github.com>
2024-12-15 21:23:51 +0100

    feat: better script output. ERROR counting.

--- readonlyhash.sh
+++ readonlyhash.sh
@@ -12,9 +12,9 @@
 EXTENSIONS_TO_AVOID="rslsi,rslsv,rslsz,rsls"
 
 # Variable for SHA-256 hash command
-SHA256_BIN="sha256sum" # linux native, macOS via brew install coreutils
+#SHA256_BIN="sha256sum" # linux native, macOS via brew install coreutils
 SHA256_BIN="shasum -a 256" # macOS native
-SHA256_BIN="openssl sha256" # pre-installed on macOS
+#SHA256_BIN="openssl sha256" # pre-installed on macOS
 
 ERROR_COUNT=0
 
@@ -43,69 +43,89 @@ generate_hash() {
     local hash_file="$(basename "$file").sha256"
     
     if [ -f "$dot_hash_file" ] || [ -f "$hash_file" ]; then
-        echo -n " -- ERROR: hash file already exists for $(basename "$file")"
+        echo "ERROR: $(basename "$file") -- hash file already exists"
         ((ERROR_COUNT++))
+        return 1  # Signal that an error occurred
     else
-        $SHA256_BIN "$file" | awk '{print $1}' > "$dot_hash_file"
-        echo -n " -- hash written"
+        local hash=$($SHA256_BIN "$file" | awk '{print $1}')
+        echo "$hash" > "$dot_hash_file"
+        echo "$hash"
+        return 0  # No error
     fi
 }
 
 # Function to delete hash files
 delete_hash() {
     local file="$1"
-    local hash_file=".$(basename "$file").sha256"
+    local dot_hash_file=".$(basename "$file").sha256"
+    local hash_file="$(basename "$file").sha256"
     
-    # Check if hash file exists before attempting to delete
-    if [ -f "$hash_file" ]; then
+    if [ -f "$dot_hash_file" ]; then
+        rm "$dot_hash_file"
+        echo "File: $(basename "$file") -- hash deleted: [$dot_hash_file]"
+        return 0  # No error
+    elif [ -f "$hash_file" ]; then
         rm "$hash_file"
-        echo -n " -- hash deleted"
+        echo "File: $(basename "$file") -- hash deleted: [$hash_file]"
+        return 0  # No error
     else
-        echo -n " -- no hash to delete"
+        echo "ERROR: $(basename "$file") -- NO hash file found"
+        ((ERROR_COUNT++))
+        return 1  # Error, hash file does not exist
     fi
 }
 
 compare_hash() {
     local file="$1"
     local hash_file=".$(basename "$file").sha256"
+    local current_hash
     
     if [ -f "$hash_file" ]; then
-        local current_hash=$($SHA256_BIN "$file" | awk '{print $1}')
+        current_hash=$($SHA256_BIN "$file" | awk '{print $1}')
         local stored_hash=$(cat "$hash_file")
         
         if [ "$current_hash" = "$stored_hash" ]; then
-            echo -n " -- hash matches"
+            #echo "File: $(basename "$file") -- hash matches: [$current_hash]"
+            echo "File: [$current_hash]: $(basename "$file") -- OK"
+            return 0  # No error
         else
-            echo -n " -- hash mismatch"
+            echo "ERROR: $(basename "$file") - hash mismatch, expected [$stored_hash], got [$current_hash]"
+            ((ERROR_COUNT++))
+            return 1  # Error, hash mismatch
         fi
     else
-        echo -n " - no hash file exists"
+        echo "ERROR: $(basename "$file") -- no hash file exists"
+        ((ERROR_COUNT++))
+        return 1  # Error, hash file does not exist
     fi
 }
 
-rename_hash() {
+manage_hash_visibility() {
     local file="$1"
-    local old_hash_file=".$(basename "$file").sha256"
-    local new_hash_file="$(basename "$file").sha256"
+    local action="$2"
+    local old_hash_file
+    local new_hash_file
     
-    if [ -f "$old_hash_file" ]; then
-        mv "$old_hash_file" "$new_hash_file"
-        echo -n " -- hash renamed"
+    if [ "$action" = "show" ]; then
+        old_hash_file=".$(basename "$file").sha256"
+        new_hash_file="$(basename "$file").sha256"
+    elif [ "$action" = "hide" ]; then
+        old_hash_file="$(basename "$file").sha256"
+        new_hash_file=".$(basename "$file").sha256"
     else
-        echo -n " -- no hash to rename"
+        echo "ERROR: $(basename "$file") -- invalid hash visibility action"
+        ((ERROR_COUNT++))
+        return 1
     fi
-}
-
-hide_hash() {
-    local file="$1"
-    local old_hash_file="$(basename "$file").sha256"
-    local new_hash_file=".$(basename "$file").sha256"
     
     if [ -f "$old_hash_file" ]; then
         mv "$old_hash_file" "$new_hash_file"
-        echo -n " -- hash hidden"
+        echo "File: $(basename "$file") -- hash ${action}: [$new_hash_file]"
+        return 0  # No error
     else
-        echo -n " -- no hash to hide"
+        echo "ERROR: $(basename "$file") -- hash file not found for ${action}ing"
+        ((ERROR_COUNT++))
+        return 1  # Error, hash file does not exist for the action
     fi
 }
 
@@ -114,43 +134,44 @@ process_directory() {
     local dir="$1"
     local write_mode="$2"
     local delete_mode="$3"
-    local rename_mode="$4"
+    local show_mode="$4"
     local hide_mode="$5"
 
     for entry in "$dir"/*; do
         if [ -d "$entry" ]; then
             # If the entry is a directory, echo it and process it recursively
             echo "Directory: $entry"
-			process_directory "$entry" "$write_mode" "$delete_mode" "$rename_mode" "$hide_mode"
-		elif [ -f "$entry" ] && [[ ! $(basename "$entry") =~ \.sha256$ ]]; then
+            process_directory "$entry" "$write_mode" "$delete_mode" "$show_mode" "$hide_mode"
+        elif [ -f "$entry" ] && [[ ! $(basename "$entry") =~ \.sha256$ ]]; then
             if check_extension "$entry"; then
-                echo "Error: Encountered file with restricted extension: $(basename "$entry")"
+                echo "ERROR: $(basename "$entry") -- encountered file with restricted extension"
                 exit 1
             else
-                echo -n "File: $(basename "$entry")"
                 if [ "$delete_mode" = "true" ]; then
                     delete_hash "$entry"
                 elif [ "$write_mode" = "true" ]; then
-                    generate_hash "$entry"
-                elif [ "$rename_mode" = "true" ]; then
-                    rename_hash "$entry"
+                    if hash=$(generate_hash "$entry"); then
+                        echo "File: $(basename "$entry") -- hash written: [$hash]"
+                    fi
+                elif [ "$show_mode" = "true" ]; then
+                    manage_hash_visibility "$entry" "show"
                 elif [ "$hide_mode" = "true" ]; then
-                    hide_hash "$entry"
+                    manage_hash_visibility "$entry" "hide"
                 else
-                    compare_hash "$entry"
+                    compare_hash "$entry" 
                 fi
-                echo  # New line after the operation message
             fi
         fi
     done
 }
 
+
 # Parse command line options
 write_mode="false"
 delete_mode="false"
-rename_mode="false"
+show_mode="false"
 hide_mode="false"
-while getopts ":dwuh" opt; do
+while getopts ":dwsh" opt; do
   case $opt in
     d)
       delete_mode="true"
@@ -158,8 +179,8 @@ while getopts ":dwuh" opt; do
     w)
       write_mode="true"
       ;;
-    u)
-      rename_mode="true"
+    s)
+      show_mode="true"
       ;;
     h)
       hide_mode="true"
@@ -171,7 +192,6 @@ while getopts ":dwuh" opt; do
   esac
 done
 
-# Use the first non-option argument as the directory, if provided
 if [ $OPTIND -le $# ]; then
     dir="${@:$OPTIND:1}"
 else
@@ -183,10 +203,10 @@ if [ -d "$dir" ]; then
     process_directory "$dir" "$write_mode" "$delete_mode" "$show_mode" "$hide_mode"
     
     if [ $ERROR_COUNT -gt 0 ]; then
-        echo "Number of ERRORs encountered: $ERROR_COUNT"
+        echo "Number of ERRORs encountered: [$ERROR_COUNT]"
         exit 1
     fi
 else
-    echo "ERROR: directory [$dir] does not exist."
+    echo "Error: Directory $dir does not exist."
     exit 1
 fi
