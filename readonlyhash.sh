02e79c85fdbb0062116228a13006befbb65c8d6a
knev <knev@users.noreply.github.com>
2024-12-16 15:29:54 +0100

    feat: rewrite branch structure; handle all cases; rename variables.

--- readonlyhash.sh
+++ readonlyhash.sh
@@ -37,109 +37,164 @@ ensure_dir() {
     fi
 }
 
-# New function for hashing
 generate_hash() {
-    local dir="$1"
-    local file="$2"
-    local hash_file="$(basename "$file").sha256"
-    local roh_hash_file="$(dirname "$file")/$ROH_DIR/$hash_file"
+    local file="$1"
+    echo $($SHA256_BIN "$file" | awk '{print $1}')
+}
 
-    if [ -f "$roh_hash_file" ]; then
-        echo "ERROR: [$dir] $(basename "$file") -- hash file [$roh_hash_file] already exists"
+stored_hash() {
+    local hash_file="$1"
+    cat "$hash_file" 2>/dev/null || echo "no_hash_file"
+}
+
+# New function for hashing
+write_hash() {
+    local dir="$1"
+    local fpath="$2"
+    local hash_fname="$(basename "$fpath").sha256"
+    #local roh_hash_file="$(dirname "$file")/$ROH_DIR/$hash_file"
+    local roh_hash_fpath="$dir/$ROH_DIR/$hash_fname"
+
+    if [ -f "$dir/$hash_fname" ]; then
+        echo "ERROR: [$dir] $(basename "$fpath") -- hash file [$dir/$hash_fname] exists and is NOT hidden"
         ((ERROR_COUNT++))
         return 1  # Signal that an error occurred
-    elif [ -f "$hash_file" ]; then
-        echo "ERROR: [$dir] $(basename "$file") -- hash file [$hash_file] already exists"
-        ((ERROR_COUNT++))
-        return 1  # Signal that an error occurred
-    else
-        local hash=$($SHA256_BIN "$file" | awk '{print $1}')
-        if echo "$hash" > "$roh_hash_file"; then
-            # echo "File: $dir $(basename "$file") -- hash written: [$hash]"
-            echo "File: [$dir] $(basename "$file") -- hash written: [$hash] -- OK"
-            return 0  # No error
-        else
-            echo "ERROR: [$dir] $(basename "$file") -- failed to write hash to [$roh_hash_file]"
-            ((ERROR_COUNT++))
-            return 1  # Signal that an error occurred
-        fi
-    fi
+	fi
+
+	local new_hash=$(generate_hash "$fpath")
+
+    if [ -f "$roh_hash_fpath" ]; then
+        local stored=$(stored_hash "$roh_hash_fpath")
+
+        if [ "$new_hash" = "$stored" ]; then
+			# echo "File: [$new_hash]: [$dir] $(basename "$fpath") -- OK"
+			return 0
+		else
+			echo "ERROR: [$dir] $(basename "$fpath") -- hash mismatch, [$roh_hash_fpath] exists with stored [$stored]"
+			((ERROR_COUNT++))
+			return 1  # Signal that an error occurred
+		fi
+	fi
+
+	if echo "$new_hash" > "$roh_hash_fpath"; then
+		echo "File: [$new_hash]: [$dir] $(basename "$fpath") -- OK"
+		return 0  # No error
+	else
+		echo "ERROR: [$dir] $(basename "$fpath") -- failed to write hash to [$roh_hash_fpath]"
+		((ERROR_COUNT++))
+		return 1  # Signal that an error occurred
+	fi
 }
 
 # Function to delete hash files
 delete_hash() {
     local dir="$1"
-    local file="$2"
-    local hash_file="$(basename "$file").sha256"
-    local roh_hash_file="$(dirname "$file")/$ROH_DIR/$hash_file"
+    local fpath="$2"
+    local hash_fname="$(basename "$fpath").sha256"
+    local roh_hash_fpath="$dir/$ROH_DIR/$hash_fname"
 
-    if [ -f "$roh_hash_file" ]; then
-        rm "$roh_hash_file"
-        echo "File: [$dir] $(basename "$file") -- hash deleted: [$roh_hash_file] -- OK"
-        return 0  # No error
-    elif [ -f "$hash_file" ]; then
-        echo "ERROR: [$dir] $(basename "$file") -- found [$hash_file]; can only delete hidden hashes"
+    if [ -f "$dir/$hash_fname" ]; then
+        echo "ERROR: [$dir] $(basename "$fpath") -- found existing hash in [$dir]; can only delete hidden hashes"
         ((ERROR_COUNT++))
         return 1  # Error, hash file does not exist
-    else
-        echo "ERROR: [$dir] $(basename "$file") -- NO hash file found in [$dir/$ROH_DIR]"
-        ((ERROR_COUNT++))
-        return 1  # Error, hash file does not exist
+	fi
+
+    if [ ! -f "$roh_hash_fpath" ]; then
+        # echo "ERROR: [$dir] $(basename "$file") -- NO hash file found in [$dir/$ROH_DIR]"
+        # ((ERROR_COUNT++))
+        # return 1  # Error, hash file does not exist
+		return 0
     fi
+
+	local computed_hash=$(generate_hash "$fpath")
+	local stored=$(stored_hash "$roh_hash_fpath")
+	
+	if [ "$computed_hash" = "$stored" ]; then
+		rm "$roh_hash_fpath"
+		echo "File: [$dir] $(basename "$fpath") -- hash file in [$dir/$ROH_DIR] deleted -- OK"
+		return 0  # No error
+	else
+		echo "ERROR: [$dir] $(basename "$fpath") -- hash mismatch, cannot delete [$roh_hash_fpath] with stored [$stored]"
+		((ERROR_COUNT++))
+		return 1  # Error, hash mismatch
+	fi
 }
 
-compare_hash() {
-    local file="$1"
-    local hash_file=".$(basename "$file").sha256"
-    local current_hash
-    
-    if [ -f "$hash_file" ]; then
-        current_hash=$($SHA256_BIN "$file" | awk '{print $1}')
-        local stored_hash=$(cat "$hash_file")
-        
-        if [ "$current_hash" = "$stored_hash" ]; then
-            #echo "File: $(basename "$file") -- hash matches: [$current_hash]"
-            echo "File: [$current_hash]: $(basename "$file") -- OK"
-            return 0  # No error
-        else
-            echo "ERROR: $(basename "$file") - hash mismatch, expected [$stored_hash], got [$current_hash]"
-            ((ERROR_COUNT++))
-            return 1  # Error, hash mismatch
-        fi
-    else
-        echo "ERROR: $(basename "$file") -- no hash file exists"
+verify_hash() {
+    local dir="$1"
+    local fpath="$2"
+    local hash_fname="$(basename "$fpath").sha256"
+    local roh_hash_fpath="$dir/$ROH_DIR/$hash_fname"
+
+    if [ ! -f "$roh_hash_fpath" ]; then
+        echo "ERROR: [$dir] $(basename "$fpath") -- no hash file exists in [$dir/$ROH_DIR]"
         ((ERROR_COUNT++))
         return 1  # Error, hash file does not exist
-    fi
+	fi
+
+	local computed_hash=$(generate_hash "$fpath")
+	local stored=$(stored_hash "$roh_hash_fpath")
+        
+	if [ "$computed_hash" = "$stored" ]; then
+		#echo "File: $(basename "$file") -- hash matches: [$computed_hash]"
+		echo "File: [$computed_hash]: [$dir] $(basename "$file") -- OK"
+		return 0  # No error
+	else
+		echo "ERROR: [$dir] $(basename "$fpath") - hash mismatch: [$roh_hash_fpath] stored [$stored], computed [$computed_hash]"
+		((ERROR_COUNT++))
+		return 1  # Error, hash mismatch
+	fi
 }
 
 manage_hash_visibility() {
     local dir="$1"
-    local file="$2"
+    local fpath="$2"
     local action="$3"
-    local hash_file="$(basename "$file").sha256"
-    local roh_hash_file="$(dirname "$file")/$ROH_DIR/$hash_file"
-    local old_hash_file
-    local new_hash_file
-    
+    local hash_fname="$(basename "$fpath").sha256"
+
+	local src_path
+    local dest_path
     if [ "$action" = "show" ]; then
-        old_hash_file=$roh_hash_file
-        new_hash_file=$hash_file
+		src_path="$dir/$ROH_DIR"
+		dest_path="$dir"
     elif [ "$action" = "hide" ]; then
-        old_hash_file=$hash_file
-        new_hash_file=$roh_hash_file
+		src_path="$dir"
+		dest_path="$dir/$ROH_DIR"
     else
-        echo "ERROR: $(basename "$file") -- invalid hash visibility action"
-        ((ERROR_COUNT++))
-        return 1
+        echo "ERROR: invalid hash visibility action"
+        exit 1
     fi
 
-    if [ -f "$old_hash_file" ]; then
-        mv "$old_hash_file" "$new_hash_file"
-        echo "File: [$dir] $(basename "$file") -- ${action}ing hash: [$new_hash_file] -- OK"
+	# src yes, dest yes -> check dest hash, if computed!=dest= err else mv
+	# src yes, dest no  -> mv
+	# src no,  dest yes -> check dest hash, if computed=dest; OK else err
+	# src no,  dest no  -> err
+	
+	local computed_hash=$(generate_hash "$fpath")
+
+	if [ -f "$src_path/$hash_fname" ]; then
+		if [ -f "$dest_path/$hash_fname" ]; then
+			local stored=$(stored_hash "$dest_path/$hash_fname")
+			if [ "$computed_hash" != "$stored_hash" ]; then
+				echo "ERROR: [$dir] $(basename "$fpath") -- existing hash file found in [$src_path], not ${action}ing"
+				((ERROR_COUNT++))
+				return 1
+			fi
+		fi
+
+        mv "$src_path/$hash_fname" "$dest_path/$hash_fname"
+        echo "File: [$dir] $(basename "$fpath") -- ${action}ing hash file [$dest_path/$hash_fname] -- OK"
         return 0  # No error
-    else
-        echo "ERROR: [$dir] $(basename "$file") -- hash file [$old_hash_file] NOT found for ${action}ing"
+	else
+		if [ -f "$dest_path/$hash_fname" ]; then
+			local stored=$(stored_hash "$dest_path/$hash_fname")
+			if [ "$computed_hash" = "$stored_hash" ]; then
+				echo "File: [$dir] $(basename "$fpath") -- ${action}ing hash file [$dest_path/$hash_fname] -- OK"
+				return 0  # No error
+			fi
+		fi
+
+        echo "ERROR: [$dir] $(basename "$fpath") -- hash file NOT found in [$src_path], not ${action}ing"
         ((ERROR_COUNT++))
         return 1  # Error, hash file does not exist for the action
     fi
@@ -172,13 +227,13 @@ process_directory() {
                 if [ "$delete_mode" = "true" ]; then
                     delete_hash "$dir" "$entry"
                 elif [ "$write_mode" = "true" ]; then
-					generate_hash "$dir" "$entry"
+					write_hash "$dir" "$entry"
                 elif [ "$show_mode" = "true" ]; then
                     manage_hash_visibility "$dir" "$entry" "show"
                 elif [ "$hide_mode" = "true" ]; then
                     manage_hash_visibility "$dir" "$entry" "hide"
                 else
-                    compare_hash "$entry" 
+                    verify_hash "$dir" "$entry" 
                 fi
             fi
         fi
@@ -228,9 +283,14 @@ if [ -d "$dir" ]; then
     process_directory "$dir" "$write_mode" "$delete_mode" "$show_mode" "$hide_mode"
     if [ $ERROR_COUNT -gt 0 ]; then
         echo "Number of ERRORs encountered: [$ERROR_COUNT]"
+		echo
         exit 1
     fi
 else
     echo "Error: Directory $dir does not exist."
+	echo
     exit 1
 fi
+
+echo "Done."
+echo
