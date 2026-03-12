7d2da5845e14898a27b2a3812032403e3931aa54
knev <knev@users.noreply.github.com>
2026-03-12 12:51:01 +0100

    fix: unit test for not clean.

--- unit_test/test_readonlyhash.sh
+++ unit_test/test_readonlyhash.sh
@@ -123,17 +123,27 @@ echo "# extract"
 
 run_test "$ROH_BIN extract < $fpath" "0" "$(escape_expected "Extracted [/Users/dev/Project-@knev/readonlyhash.sh.git/Fotos [space]/2003/.roh.git] from [_.roh.git.zip].*Removed [/Users/dev/Project-@knev/readonlyhash.sh.git/Fotos [space]/2003/_.roh.git.zip]")"
 
-run_test "$ROH_BIN verify < $fpath" "0" "ERROR" "true"
+
+# verify
+echo
+echo "# verify"
+run_test "$ROH_BIN verify < $fpath" "0" "ERROR:" "true"
 
 echo "0000000000000000000000000000000000000000000000000000000000000000" > "2002.ro/$ROH_DIR/2002_FIRE!/Untitled-001.jpg.$HASH"
 run_test "$ROH_BIN verify < $fpath" "1" "$(escape_expected "ERROR: hash mismatch:.*stored [0000000000000000000000000000000000000000000000000000000000000000][$PWD/2002.ro/.roh.git/2002_FIRE!/Untitled-001.jpg.sha256].* computed [816d2fd63482855aaadd92294ef84c4a415945df194734c8834e06dd57538dc4][$PWD/2002.ro/2002_FIRE!/Untitled-001.jpg]")"
 echo "816d2fd63482855aaadd92294ef84c4a415945df194734c8834e06dd57538dc4" > "2002.ro/$ROH_DIR/2002_FIRE!/Untitled-001.jpg.$HASH"
 
 echo "0000000000000000000000000000000000000000000000000000000000000000" > "2002.ro/$ROH_DIR/2002_FIRE!/.HIDDEN_FILE"
-run_test "$ROH_BIN verify < $fpath" "1" "$(escape_expected "ERROR: local repo [$PWD/2002.ro/$ROH_DIR] not clean")"
+run_test "$ROH_BIN verify < $fpath" "1" "$(escape_expected "ERROR: directory [/Users/dev/Project-@knev/readonlyhash.sh.git/2002.ro/.roh.git/2002_FIRE!] contains hidden entries")"
 rm "2002.ro/$ROH_DIR/2002_FIRE!/.HIDDEN_FILE"
 
-run_test "$ROH_BIN verify < $fpath" "0" "ERROR" "true"
+echo "NOT CLEAN" > "2002.ro/not-clean.txt"
+$FPATH_BIN write "2002.ro" >/dev/null 2>&1
+run_test "$ROH_BIN verify < $fpath" "1" "$(escape_expected "ERROR: local repo [$PWD/2002.ro/$ROH_DIR] not clean")"
+rm "2002.ro/not-clean.txt"
+rm "2002.ro/$ROH_DIR/not-clean.txt.$HASH"
+
+run_test "$ROH_BIN verify < $fpath" "0" "ERROR:" "true"
 
 # extract && verify
 echo
